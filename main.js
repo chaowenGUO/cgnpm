7b68d20ac6cce92b02c1733f7ebab92c8e43a6c0
chaowen guo <chaowen.guo1@gmail.com>
2020-07-08 02:06:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ import a from './index.js'
 import path from 'path'
 globalThis.console.log(a)
 globalThis.console.log(path.basename('/foo/bar/baz/asdf/quux.html'))
+import events from 'events'
+const emitter = new events.EventEmitter()
+emitter.on('sayHi', function(someone){
+    console.log("我是", someone)
+})
+emitter.on('sayHi', function(someone){
+    console.log("我就是", someone)
+})
+emitter.emit('sayHi', 'jerry')
+globalThis.console.log(a)
